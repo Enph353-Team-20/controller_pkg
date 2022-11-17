aa15132841e5c71acf11b98bc9884d74bd69e648
TWilson05 <tb.wilson05@gmail.com>
2022-11-16 17:03:42 -0800

    robot moving with camera

--- src/node/PID_motion.py
+++ src/node/PID_motion.py
@@ -24,7 +24,10 @@ class image_converter:
     # initiate the class
     def __init__(self):
         self.bridge = CvBridge()
+        # subscribe to camera
         self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,self.callback)
+        # publish to movement
+        self.cmd_pub = rospy.Publisher('/R1/cmd_vel', Twist, queue_size=1)
 
     # the loop that will read from the camera and get the robot to move
     def callback(self,data):
@@ -38,6 +41,17 @@ class image_converter:
             print(e)
         cv2.imshow("Image window", cv_image)
         cv2.waitKey(3)
+
+        # create the move object
+        move = Twist()
+
+        move.angular.z = 3
+
+        # publish the move object
+        try:
+            self.cmd_pub.publish(move)
+        except CvBridgeError as e:
+            print(e)
 
 
 
@@ -53,81 +67,3 @@ def main(args):
 
 if __name__ == '__main__':
     main(sys.argv)
-
-
-### Stuff to make camera work
-
-# def callback(data):
-#     """Analyzes robot camera image to determine location of the line on the ground.
-#     Args:
-#         data (Image): Image pulled from camera topic
-#     """
-#     try:
-#       cv_image = bridge.imgmsg_to_cv2(data, "mono8")
-#     except CvBridgeError as e:
-#       print(e)
-#     cv2.imshow("Image window", cv_image)
-#     cv2.waitKey(3)
-
-# def main(args):
-#     image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,callback)
-#     rospy.init_node('camera_test', anonymous=True)
-#     try:
-#         rospy.spin()
-#     except KeyboardInterrupt:
-#         print("Shutting down")
-#         cv2.destroyAllWindows()
-
-# if __name__ == '__main__':
-#     main(sys.argv)
-
-
-
-
-
-
-### Stuff to move robot
-
-# rospy.init_node('pid_node')
-# pub = rospy.Publisher('/R1/cmd_vel', Twist, 
-#     queue_size=1)
-
-# def updateCentroid(data):
-#   """
-#   Pulls location of the line from centroid topic, and calculates new driving directions
-#   """
-# #   centroidX = float(data.data)
-# #   move = Twist()
-# #   if (centroidX > 10 and centroidX < (xWidth - 10)):
-# #     move.linear.x = 1
-# #     move.angular.z = (xWidth/2 - centroidX)/corrVal
-# #   else:
-# #     move.linear.x = 0.5
-# #     move.angular.z = 1
-# #   pub.publish(move)
-
-# #    move = Twist()
-# #    move.linear.x = 1
-# #    move.angular.z = 0.3
-# #    pub.publish(move)
-
-
-# def updateCorrVal(data):
-#     """Pulls correction constant from corrVal topic. Only applicable during runtime.
-#     Args:
-#         data (std_msgs.msgs/Float32): New value
-#     """
-#     if (float(data.data) != 0):
-#         corrVal = float(data.data)
-
-
-# # sub = rospy.Subscriber('line_x', Int16, updateCentroid)
-# # sub = rospy.Subscriber('corrVal', Float32, updateCorrVal)
-# rate = rospy.Rate(2)
-
-# while not rospy.is_shutdown():
-#     move = Twist()
-#     # move.linear.x = 1
-#     move.angular.z = 0.3
-#     pub.publish(move)
-#     rate.sleep()
