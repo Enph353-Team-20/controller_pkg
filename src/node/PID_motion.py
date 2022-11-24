071c186743fe6aa5e624c02bb9e6d95a2ccaa6aa
TWilson05 <tb.wilson05@gmail.com>
2022-11-23 18:56:47 -0800

    robot doing loops

--- src/node/PID_motion.py
+++ src/node/PID_motion.py
@@ -13,12 +13,10 @@ from geometry_msgs.msg import Twist
 from std_msgs.msg import Int16, Float32
 
 
-# define constants
-# TODO update constants
-xWidth = 800
-centroidX = 300
-corrVal = 50.0
+# start time variable
+start_time = time.time()
 
+# define constants
 threshold_val = 150
 
 
@@ -37,6 +35,8 @@ class image_converter:
         self.image_sub = rospy.Subscriber("/R1/pi_camera/image_raw",Image,self.callback)
         # publish to movement
         self.cmd_pub = rospy.Publisher('/R1/cmd_vel', Twist, queue_size=1)
+        # # publish license plates
+        # self.plate_pub = rospy.Publisher('/license_plate', String, queue_size=1)
 
     # the loop that will read from the camera and get the robot to move
     def callback(self,data):
@@ -45,13 +45,20 @@ class image_converter:
             data (Image): Image pulled from camera topic
         """
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "mono8")
-            cv_colour = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            # cv_image = self.bridge.imgmsg_to_cv2(data, "mono8")
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
         except CvBridgeError as e:
             print(e)
 
+
+        # masking the hsv
+        lower = np.array([0,0,100])
+        upper = np.array([0,0,255])
+        mask = cv2.inRange(hsv, lower, upper)
+
         # trying something out here
-        cv_image = cv_colour[:,:,0]
+        # cv_image = cv_colour[:,:,0]
 
         # testing for threshold
         ret, thresh_img = cv2.threshold(cv_image, threshold_val, 255, cv2.THRESH_BINARY_INV)
@@ -61,13 +68,13 @@ class image_converter:
         image_width = cv_image.shape[1]
 
         # variable for which height to partition the screen into
-        look_height = int(0.7*image_height)
-        above_look = int(0.6*image_height)
+        look_height = int(0.8*image_height)
+        above_look = int(0.7*image_height)
         bottom_look = int(1*image_height)
 
 
         # find the center of the line
-        cXR = self.findLineCentroidX(cv_image, (look_height, bottom_look),(int(image_width/2),int(image_width)-1-200))
+        cXR = self.findLineCentroidX(cv_image, (look_height, bottom_look),(int(image_width/2)-200,int(image_width)-1))
         cXL = self.findLineCentroidX(cv_image, (look_height, bottom_look), (200,int(image_width/2)))
         if cXR == -1:
             cXR = self.cXR_last
@@ -105,27 +112,33 @@ class image_converter:
 
         # attempt at a PID
         angMax = 4
-        move.linear.x = 0.15
+        move.linear.x = 0.2
 
         # move.linear.x = 0
         # move.angular.z = 0 # for reseting the robot
 
-        slope_turn_constant = 30
-
+        slope_turn_constant = 80
 
         if slopeL < slope_turn_constant and slopeR < -slope_turn_constant:
             # left line go bye bye (steer with right)
-            move.angular.z = (1-4*(cXR-640)/image_width)*angMax
-            cv2.putText(img=thresh_img, text="Right", org=(1200, 20), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.7, color=(0, 255, 0),thickness=1)
+            move.angular.z = 2*(1-4*(cXR-640)/image_width)*angMax
+            cv2.putText(img=thresh_img, text="Right", org=(1200, 400), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.7, color=(0, 180, 0),thickness=1)
         elif slopeL > slope_turn_constant and slopeR > -slope_turn_constant:
             # right line go bye bye (steer with left)
-            move.angular.z = (1-4*(cXL)/image_width)*angMax
-            cv2.putText(img=thresh_img, text="Left", org=(20, 20), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.7, color=(0, 255, 0),thickness=1)
+            move.angular.z = 2*(1-4*(cXL)/image_width)*angMax
+            cv2.putText(img=thresh_img, text="Left", org=(20, 400), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.7, color=(0, 180, 0),thickness=1)
         else:
             # both lines in sight
             cX = int((cXR+cXL)/2)
             move.angular.z = (1-2*cX/image_width)*angMax
-            cv2.putText(img=thresh_img, text="Both", org=(600, 20), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.7, color=(0, 255, 0),thickness=1)
+            cv2.putText(img=thresh_img, text="Both", org=(600, 400), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.7, color=(0, 180, 0),thickness=1)
+
+
+
+        # # make sure we turn left
+        # current_time = time.time()
+        # if current_time - start_time < 10:
+        #     move.angular.z = (1-4*(cXL)/image_width)*angMax
 
         
         
@@ -142,9 +155,10 @@ class image_converter:
         # channel = 0
         # ret_col, thresh_img_col = cv2.threshold(cv_colour[:,:,channel], threshold_val, 255, cv2.THRESH_BINARY_INV)
 
-        cv2.imshow("Image window", thresh_img)
+        # cv2.imshow("Image window", thresh_img)
+        cv2.imshow("Image window", mask)
         # cv2.imshow("Image window", thresh_img_col)
-        cv2.waitKey(3)
+        cv2.waitKey(1)
 
     # function to find the center of the line
     def findLineCentroidX(self, img, y_range, x_range):
@@ -156,9 +170,20 @@ class image_converter:
         Returns:
             int: Centroid of line
         """
-        cropped_img = img[y_range[0]:y_range[1],x_range[0]:x_range[1]]
+        # img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        ret, thresh_img = cv2.threshold(cropped_img, threshold_val, 255, cv2.THRESH_BINARY)
+        cropped_img = img[y_range[0]:y_range[1],x_range[0]:x_range[1],:]
+
+
+        hsv_img = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2HSV)
+        # masking the hsv
+        lower = np.array([0,0,100])
+        upper = np.array([0,0,255])
+        thresh_img = cv2.inRange(hsv_img, lower, upper)
+
+
+
+        # ret, thresh_img = cv2.threshold(cropped_img, threshold_val, 255, cv2.THRESH_BINARY)
 
         # blur image to reduce noise
         thresh_img = cv2.GaussianBlur(thresh_img,(5,5),0)
@@ -169,7 +194,7 @@ class image_converter:
         except ZeroDivisionError:
             cX = -1
 
-        return cX + x_range[0]
+        return cX + x_range[0]-40
         
 
 
@@ -178,6 +203,7 @@ class image_converter:
 def main(args):
     rospy.init_node('PID_motion', anonymous=True)
     ic = image_converter()
+    # ic.plate_pub.publish(test_plate)
     try:
         rospy.spin()
     except KeyboardInterrupt:
