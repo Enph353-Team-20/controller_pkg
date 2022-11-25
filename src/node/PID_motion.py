f288d963a2aa1cd27a9af7563ee29955e80feae3
TWilson05 <tb.wilson05@gmail.com>
2022-11-24 21:26:05 -0800

    cleaned up code

--- src/node/PID_motion.py
+++ src/node/PID_motion.py
@@ -17,7 +17,7 @@ from std_msgs.msg import Int16, Float32
 start_time = time.time()
 
 # define constants
-threshold_val = 150
+white_threshold = 100
 
 
 # create class to run the camera/movement loop
@@ -53,15 +53,12 @@ class image_converter:
 
 
         # masking the hsv
-        lower = np.array([0,0,100])
+        lower = np.array([0,0,white_threshold])
         upper = np.array([0,0,255])
-        mask = cv2.inRange(hsv, lower, upper)
+        thresh_img = cv2.inRange(hsv, lower, upper)
 
-        # trying something out here
-        # cv_image = cv_colour[:,:,0]
-
-        # testing for threshold
-        ret, thresh_img = cv2.threshold(cv_image, threshold_val, 255, cv2.THRESH_BINARY_INV)
+        # # include for binary threshold
+        # ret, thresh_img = cv2.threshold(cv_image, threshold_val, 255, cv2.THRESH_BINARY_INV)
 
         # get dimensions of the image
         image_height = cv_image.shape[0]
@@ -97,97 +94,77 @@ class image_converter:
         else:
             self.cXLa_last = cXLa
 
-        # find the slope
+        # find the difference between centroids in the upper and lower windows
         slopeR = cXRa-cXR
         slopeL = cXLa-cXL
 
-
-        # make a circle on the center of the image
-        cv2.circle(thresh_img, (cXR, 300), 16, (0,255,0), -1)
-        cv2.circle(thresh_img, (cXL, 300), 16, (0,255,0), -1)
-        cv2.circle(thresh_img, (int((cXR+cXL)/2), 300), 25, (0,255,0), -1)
+        # plot left and right centroids on the image
+        cv2.circle(thresh_img, (cXR, 300), 16, (255,255,255), -1)
+        cv2.circle(thresh_img, (cXL, 300), 16, (255,255,255), -1)
+        cv2.circle(thresh_img, (int((cXR+cXL)/2), 300), 25, (255,255,255), -1)
 
         # create the move object
         move = Twist()
 
-        # attempt at a PID
+        # PID turning values and speed
         angMax = 4
         move.linear.x = 0.2
 
-        # move.linear.x = 0
-        # move.angular.z = 0 # for reseting the robot
+        # constant to determine which line to follow (big impact on PID performance)
+        slope_turn_constant = 20
 
-        slope_turn_constant = 80
-
+        # determine which line to follow with PID
         if slopeL < slope_turn_constant and slopeR < -slope_turn_constant:
             # left line go bye bye (steer with right)
             move.angular.z = 2*(1-4*(cXR-640)/image_width)*angMax
-            cv2.putText(img=thresh_img, text="Right", org=(1200, 400), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.7, color=(0, 180, 0),thickness=1)
+            cv2.putText(img=thresh_img, text="Right", org=(1200, 100), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.7, color=(255, 255, 255),thickness=1)
         elif slopeL > slope_turn_constant and slopeR > -slope_turn_constant:
             # right line go bye bye (steer with left)
             move.angular.z = 2*(1-4*(cXL)/image_width)*angMax
-            cv2.putText(img=thresh_img, text="Left", org=(20, 400), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.7, color=(0, 180, 0),thickness=1)
+            cv2.putText(img=thresh_img, text="Left", org=(20, 100), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.7, color=(255, 255, 255),thickness=1)
         else:
             # both lines in sight
             cX = int((cXR+cXL)/2)
             move.angular.z = (1-2*cX/image_width)*angMax
-            cv2.putText(img=thresh_img, text="Both", org=(600, 400), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.7, color=(0, 180, 0),thickness=1)
-
-
-
-        # # make sure we turn left
-        # current_time = time.time()
-        # if current_time - start_time < 10:
-        #     move.angular.z = (1-4*(cXL)/image_width)*angMax
-
+            cv2.putText(img=thresh_img, text="Both", org=(600, 100), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.7, color=(255, 255, 255),thickness=1)
         
-        
-
         # publish the move object
         try:
             self.cmd_pub.publish(move)
         except CvBridgeError as e:
             print(e)
 
-        # blur image to reduce noise
-        thresh_img = cv2.GaussianBlur(thresh_img,(5,5),0)
-
-        # channel = 0
-        # ret_col, thresh_img_col = cv2.threshold(cv_colour[:,:,channel], threshold_val, 255, cv2.THRESH_BINARY_INV)
-
-        # cv2.imshow("Image window", thresh_img)
-        cv2.imshow("Image window", mask)
-        # cv2.imshow("Image window", thresh_img_col)
+        # display troubleshooting screen
+        cv2.imshow("Image window", thresh_img)
         cv2.waitKey(1)
 
-    # function to find the center of the line
+
     def findLineCentroidX(self, img, y_range, x_range):
         """Finds X-coordinate centroid of the line
         Checks pixels across entire width of image and between y values defined in y_range
         Args:
             img (Image): Image to analyze
-            y_range (Tuple(int, int)): Y range of pixels to search. 
+            y_range (Tuple(int, int)): Y range of pixels to search.
+            x_raneg (Tuple(int, int)): X range of pixels to search.
         Returns:
             int: Centroid of line
         """
-        # img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+        # blur image to reduce noise
+        img = cv2.GaussianBlur(img,(5,5),0)
+
+        # crop to look at desired window
         cropped_img = img[y_range[0]:y_range[1],x_range[0]:x_range[1],:]
 
-
+        # convert image to HSV format
         hsv_img = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2HSV)
-        # masking the hsv
-        lower = np.array([0,0,100])
+
+        # masking the hsv to filter for white lines
+        lower = np.array([0,0,white_threshold])
         upper = np.array([0,0,255])
         thresh_img = cv2.inRange(hsv_img, lower, upper)
 
-
-
-        # ret, thresh_img = cv2.threshold(cropped_img, threshold_val, 255, cv2.THRESH_BINARY)
-
-        # blur image to reduce noise
-        thresh_img = cv2.GaussianBlur(thresh_img,(5,5),0)
-
+        # get moments
         M = cv2.moments(thresh_img)
         try:
             cX = int(M["m10"] / M["m00"])
