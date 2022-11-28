e51743996a0a6586d67d315d4ec42c84d34f4da3
TWilson05 <tb.wilson05@gmail.com>
2022-11-28 11:44:48 -0800

    driving on the inside now but jank af

--- src/node/PID_motion.py
+++ src/node/PID_motion.py
@@ -31,6 +31,7 @@ class image_converter:
     detection_time = 0
     start_time = time.time()
     delay_time = 0
+    pedestrian_count = 0
 
     # initiate the class
     def __init__(self):
@@ -82,6 +83,14 @@ class image_converter:
         # PID turning values and speed
         angMax = 4
         move.linear.x = 0.2
+
+        lower_car = np.array([0,0,50])
+        upper_car = np.array([360,0,75])
+
+        # lower_car = np.array([0,30,0])
+        # upper_car = np.array([12,254,254])
+
+        car_thresh = cv2.inRange(hsv, lower_car, upper_car)
 
 
 
@@ -162,6 +171,11 @@ class image_converter:
             red_upper = np.array([6,255,255])
             red_mask = cv2.inRange(hsv,red_lower,red_upper)
 
+            # see if it has made a loop
+            if self.pedestrian_count > 1 and time.time() > self.detection_time+9:
+                self.state = "turn to inner"
+                self.delay_time = time.time()
+
             # stop if the crosswalk is there
             red_center = self.findCentroidY(red_mask)
             if red_center > 550:
@@ -179,13 +193,107 @@ class image_converter:
             # look for the pedestrian
             error_val = 0.5
             img_val = self.averageImageValue(gray,640,360,100)
-            if time.time()>self.delay_time+2:
+            if time.time()>self.delay_time+1:
                 if img_val > self.img_val_last+error_val or img_val<self.img_val_last-error_val:
                     print("pedestrian")
+                    self.pedestrian_count += 1
                     cv2.circle(thresh_img, (640, 400), 49, (255,255,255), -1)
                     self.state = "drive"
                     self.detection_time = time.time()
             self.img_val_last = img_val
+
+        elif self.state == "turn to inner":
+
+            # find left centroid
+            cXL = self.findLineCentroidX(cv_image, (look_height, bottom_look), (200,int(image_width/2)))
+            if cXL == -1:
+                cXL = self.cXL_last
+            else:
+                self.cXL_last = cXL
+
+            # PID steering with left line
+            move.angular.z = 2*(1-4*(cXL)/image_width)*angMax
+
+            # drive long enough to make a turn
+            if time.time() > self.delay_time + 3.5:
+                self.delay_time = time.time()
+                self.state = "car"
+
+        elif self.state == "car":
+
+            # stop the car
+            move.angular.z = 0
+            move.linear.x = 0
+
+            # look for the car
+            error_val = 0.5
+            img_val = self.averageImageValue(gray,640,360,100)
+            if time.time()>self.delay_time+1:
+                if img_val > self.img_val_last+error_val or img_val<self.img_val_last-error_val:
+                    print("car")
+                    cv2.circle(thresh_img, (640, 400), 49, (255,255,255), -1)
+                    self.state = "inner loop"
+                    self.detection_time = time.time()
+            self.img_val_last = img_val
+
+        elif self.state == "inner loop":
+
+            # find the center of the line
+            cXR = self.findLineCentroidX(cv_image, (look_height, bottom_look),(int(image_width/2)-200,int(image_width)-1))
+            cXL = self.findLineCentroidX(cv_image, (look_height, bottom_look), (200,int(image_width/2)))
+            cXc = self.findCarCenter(car_thresh,(look_height, bottom_look), (int(image_width/2)-600,int(image_width/2)+600))
+            if cXR == -1:
+                cXR = self.cXR_last
+            else:
+                self.cXR_last = cXR
+            if cXL == -1:
+                cXL = self.cXL_last
+            else:
+                self.cXL_last = cXL
+
+            # find the center of the line for the screen above
+            cXRa = self.findLineCentroidX(cv_image, (above_look, look_height),(int(image_width/2),int(image_width)-1-200))
+            cXLa = self.findLineCentroidX(cv_image, (above_look, look_height), (200,int(image_width/2)))
+            if cXRa == -1:
+                cXRa = 640
+            else:
+                self.cXRa_last = cXRa
+            if cXLa == -1:
+                cXLa = 640
+            else:
+                self.cXLa_last = cXLa
+
+            # find the difference between centroids in the upper and lower windows
+            slopeR = cXRa-cXR
+            slopeL = cXLa-cXL
+
+            # plot left and right centroids on the image
+            cv2.circle(thresh_img, (cXR, 300), 16, (255,255,255), -1)
+            cv2.circle(thresh_img, (cXL, 300), 16, (255,255,255), -1)
+            cv2.circle(thresh_img, (int((cXR+cXL)/2), 300), 25, (255,255,255), -1)
+
+            # constant to determine which line to follow (big impact on PID performance)
+            slope_turn_constant = 10000
+
+            # start driving a little after it sees the car so it doesn't run into it
+            if time.time() > self.detection_time + 1:
+                # determine which line to follow with PID
+                if slopeL > slope_turn_constant and slopeR > -slope_turn_constant:
+                    # right line go bye bye (steer with left)
+                    move.angular.z = 2*(1-4*(cXL)/image_width)*angMax
+                    cv2.putText(img=thresh_img, text="Left", org=(20, 100), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.7, color=(255, 255, 255),thickness=1)
+                elif slopeL < slope_turn_constant and slopeR < -slope_turn_constant:
+                    # left line go bye bye (steer with right)
+                    move.angular.z = 2*(1-4*(cXR-640)/image_width)*angMax
+                    cv2.putText(img=thresh_img, text="Right", org=(1200, 100), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.7, color=(255, 255, 255),thickness=1)
+                else:
+                    # follow the car
+                    move.linear.x = 0.3
+                    move.angular.z = (1-2*cXc/image_width)*angMax
+                    cv2.putText(img=thresh_img, text="Car", org=(600, 100), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=0.7, color=(255, 255, 255),thickness=1)
+            else:
+                move.linear.x = 0
+                move.angular.z = 0
 
         
         
@@ -257,6 +365,19 @@ class image_converter:
 
         return np.mean(cropped_img)
         
+    def findCarCenter(self, img, y_range, x_range):
+
+        # crop to look at desired window
+        cropped_img = img[y_range[0]:y_range[1],x_range[0]:x_range[1]]
+
+        # get moments
+        M = cv2.moments(cropped_img)
+        try:
+            cX = int(M["m10"] / M["m00"])
+        except ZeroDivisionError:
+            cX = -1
+
+        return cX
 
 
 
